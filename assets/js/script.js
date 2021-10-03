3c65eba74234cb2012d8bc5fa23bccd19b031117
uzor1997 <66738486+uzor1997@users.noreply.github.com>
2021-10-03 16:11:32 +0100

    making a lot

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 const modals = document.querySelector(".pop-up");
 const Closemodals = document.querySelector(".pop-up i");
 const allocationImage = document.querySelector("#allocation-image");
+const check = document.querySelector("#check");
+const navLinks = document.querySelectorAll(".nav-link");
+const dropdownLinks = document.querySelectorAll(".dropdown-link");
 
 const imgArray = [
   "assets/images/royale-images/ALL UYO I.jpg",
@@ -20,5 +23,11 @@ window.addEventListener("DOMContentLoaded", () => {
 
   Closemodals.addEventListener("click", () => {
     modals.classList.remove("appear");
+  });
+
+  dropdownLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      check.checked = false;
+    });
   });
 });
