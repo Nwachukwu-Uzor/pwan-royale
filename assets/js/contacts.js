2b67d53989446d7842865e46cd41fb4a6e1147b3
Buzy Dev <uzor1997@gmail.com>
2021-10-27 15:30:41 +0100

    blabla:

--- assets/js/contacts.js
+++ assets/js/contacts.js
@@ -7,17 +7,24 @@ contactForm.addEventListener("submit", (event) => {
   const email = document.querySelector("#email").value;
   const message = document.querySelector("#message").value;
 
-  sendEmail(name, email, message);
+  const validate = name.trim().length > 4 && message.trim().length > 20;
+
+  if (validate) {
+    sendEmail(name, email, message);
+    contactForm.reset();
+  } else {
+    alert("Please use the right credentials");
+  }
 });
 
-const sendEmail = (name, email, message) => {
+function sendEmail(name, email, message) {
   Email.send({
     Host: "smtp.gmail.com",
     Username: "uzor1997@gmail.com",
-    password: "bemzqraelafthtmx",
+    Password: "bemzqraelafthtmx",
     To: "uzor1997@gmail.com",
     From: "uzor1997@gmail.com",
     Subject: `${name} sent you a message from your website`,
     Body: `Name: ${name} <br /> Email: ${email} <br /> Message: ${message}`,
-  }).then((message) => alert("message successfully sent"));
-};
+  }).then((message) => alert("Message Successfully sent"));
+}
